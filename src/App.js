22bf9924bbbf92abd028a8eac4e9a1612d077567
NateDoesMath <natebeal18@gmail.com>
2021-09-21 20:46:20 -0700

    added frontend

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import SchedList from './components/SchedList';
+import Semester from './components/Semester';
+import AddStudent from './components/AddStudent';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+       <Switch>
+        <Route exact path='/' component={Semester} />
+        <Route path='/schedule' component={SchedList} />
+        <Route path='/AddStudent' component={AddStudent} />
+       </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
